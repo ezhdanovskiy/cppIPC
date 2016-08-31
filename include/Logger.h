285a39d9d3895c63f204c8bbf447c2990372fc7a
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-08-31 11:08:05 +0300

    compatibility with FreeBSD

--- include/Logger.h
+++ include/Logger.h
@@ -4,7 +4,12 @@
 #include <unistd.h>
 #include <thread>
 
-#define LOG_BEGIN "[" << getpid() << ":" << std::this_thread::get_id() << "]\t"
+#if defined(__linux__)
+    #define LOG_BEGIN "[" << getpid() << std::hex << ":0x" << std::this_thread::get_id() << std::dec << "]\t"
+#else
+    #define LOG_BEGIN "[" << getpid() << ":" << std::this_thread::get_id() << "]\t"
+#endif
+
 #define LOG_END "\t\t\t[" << __FILE__ << ":" << __LINE__ << "]" << std::endl;
 #define LOG(chain) std::cout << LOG_BEGIN << chain << LOG_END
 #define LOG1(el) std::cout << LOG_BEGIN << #el << "=" << (el) << LOG_END
