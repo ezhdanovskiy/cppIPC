dd839d0c293685a995eaeddbc55351ff1481d6d7
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-08-31 11:00:58 +0300

    SharedMemory works

--- SharedMemory/main.cpp
+++ SharedMemory/main.cpp
@@ -1,10 +1,43 @@
-#include <iostream>
-#include <future>
-#include <unistd.h>
-#include <sstream>
-#include <vector>
-#include <atomic>
+#include "Logger.h"
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <sys/wait.h>  /* Needed for the wait function */
+#include <unistd.h>    /* needed for the fork function */
+#include <string.h>    /* needed for the strcat function */
+
+#define SHMSIZE 27
 
 int main() {
+    int shmid;
+    char *shm;
+
+    if(fork() == 0) {
+        shmid = shmget(2009, SHMSIZE, 0);
+        shm = (char*)shmat(shmid, 0, 0);
+        char *s = (char *) shm;
+        *s = '\0';  /* Set first location to string terminator, for later append */
+        int i;
+        for(i=0; i<5; i++) {
+            int n;  /* Variable to get the number into */
+            printf("Enter number<%i>: ", i);
+            scanf("%d", &n);
+            sprintf(s, "%s%d", s, n);  /* Append number to string */
+        }
+        LOG1(shm);
+        shmdt(shm);
+    }
+    else {
+        /* Variable s removed, it wasn't used */
+        /* Removed first call to wait as it held up parent process */
+        shmid = shmget(2009, SHMSIZE, 0666 | IPC_CREAT);
+        shm = (char*)shmat(shmid, 0, 0);
+        wait(NULL);
+        LOG1(shm);
+        shmdt(shm);
+        shmctl(shmid, IPC_RMID, NULL);
+    }
     return 0;
 }
--- include/Logger.h
+++ include/Logger.h
@@ -2,8 +2,9 @@
 
 #include <iostream>
 #include <unistd.h>
+#include <thread>
 
-#define LOG_BEGIN "[" << getpid() << "]\t"
+#define LOG_BEGIN "[" << getpid() << ":" << std::this_thread::get_id() << "]\t"
 #define LOG_END "\t\t\t[" << __FILE__ << ":" << __LINE__ << "]" << std::endl;
 #define LOG(chain) std::cout << LOG_BEGIN << chain << LOG_END
 #define LOG1(el) std::cout << LOG_BEGIN << #el << "=" << (el) << LOG_END
