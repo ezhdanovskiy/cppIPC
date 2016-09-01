1c3871aa4941b4088d15568d0964ae321fbff036
Evgeny Zhdanovskiy <zhdanovskiy@playform.com>
2016-09-01 11:43:38 +0300

    SharedMemory : refactoring

--- SharedMemory/main.cpp
+++ SharedMemory/main.cpp
@@ -4,26 +4,31 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <stdio.h>
-#include <sys/wait.h>  /* Needed for the wait function */
-#include <unistd.h>    /* needed for the fork function */
-#include <string.h>    /* needed for the strcat function */
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <err.h>
 
 #define SHMSIZE 27
+#define SHMKEY 2009
 
 int main() {
+    srand(time(0));
     int shmid;
     char *shm;
 
     if(fork() == 0) {
-        shmid = shmget(2009, SHMSIZE, 0);
+        shmid = shmget(SHMKEY, SHMSIZE, 0);
+        LOG1(shmid);
+        if(shmid == -1){
+            err(1, "shmget");
+        }
         shm = (char*)shmat(shmid, 0, 0);
         char *s = (char *) shm;
         *s = '\0';  /* Set first location to string terminator, for later append */
         int i;
         for(i=0; i<5; i++) {
-            int n;  /* Variable to get the number into */
-            printf("Enter number<%i>: ", i);
-            scanf("%d", &n);
+            int n = rand() % 100 ;
             sprintf(s, "%s%d", s, n);  /* Append number to string */
         }
         LOG1(shm);
@@ -32,7 +37,12 @@ int main() {
     else {
         /* Variable s removed, it wasn't used */
         /* Removed first call to wait as it held up parent process */
-        shmid = shmget(2009, SHMSIZE, 0666 | IPC_CREAT);
+//        usleep(1000);
+        shmid = shmget(SHMKEY, SHMSIZE, 0666 | IPC_CREAT);
+        LOG1(shmid);
+        if(shmid == -1){
+            err(1, "shmget");
+        }
         shm = (char*)shmat(shmid, 0, 0);
         wait(NULL);
         LOG1(shm);
